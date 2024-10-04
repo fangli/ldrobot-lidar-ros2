bc25911f08119c68e64fc211c2e587ebeb757033
Fang Li <surivlee@gmail.com>
2024-10-03 21:32:57 -0700

    initial jazzy fixes

--- ldlidar_node/launch/ldlidar_slam.launch.py
+++ ldlidar_node/launch/ldlidar_slam.launch.py
@@ -19,100 +19,153 @@ from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node, LifecycleNode
+from launch.actions import (
+    DeclareLaunchArgument,
+    EmitEvent,
+    LogInfo,
+    RegisterEventHandler,
+)
+from launch.conditions import IfCondition
+from launch.events import matches_action
+from launch.substitutions import AndSubstitution, LaunchConfiguration, NotSubstitution
+from launch_ros.actions import LifecycleNode
+from launch_ros.event_handlers import OnStateTransition
+from launch_ros.events.lifecycle import ChangeState
+from lifecycle_msgs.msg import Transition
 
 
 def generate_launch_description():
-    
-    node_name = LaunchConfiguration('node_name')
+    autostart = LaunchConfiguration("autostart")
+    node_name = LaunchConfiguration("node_name")
+    use_lifecycle_manager = LaunchConfiguration("use_lifecycle_manager")
+    use_sim_time = LaunchConfiguration("use_sim_time")
+
+    declare_autostart_cmd = DeclareLaunchArgument(
+        "autostart",
+        default_value="true",
+        description="Automatically startup the slamtoolbox. "
+        "Ignored when use_lifecycle_manager is true.",
+    )
+    declare_use_lifecycle_manager = DeclareLaunchArgument(
+        "use_lifecycle_manager",
+        default_value="false",
+        description="Enable bond connection during node activation",
+    )
+    declare_use_sim_time_argument = DeclareLaunchArgument(
+        "use_sim_time", default_value="false", description="Use simulation/Gazebo clock"
+    )
 
     # Lifecycle manager configuration file
     lc_mgr_config_path = os.path.join(
-        get_package_share_directory('ldlidar_node'),
-        'params',
-        'lifecycle_mgr_slam.yaml'
+        get_package_share_directory("ldlidar_node"), "params", "lifecycle_mgr_slam.yaml"
     )
 
     # SLAM Toolbox configuration for LDLidar
     slam_config_path = os.path.join(
-        get_package_share_directory('ldlidar_node'),
-        'params',
-        'slam_toolbox.yaml'
+        get_package_share_directory("ldlidar_node"), "params", "slam_toolbox.yaml"
     )
 
     # Lifecycle manager node
     lc_mgr_node = Node(
-        package='nav2_lifecycle_manager',
-        executable='lifecycle_manager',
-        name='lifecycle_manager',
-        output='screen',
+        package="nav2_lifecycle_manager",
+        executable="lifecycle_manager",
+        name="lifecycle_manager",
+        output="screen",
         parameters=[
             # YAML files
             lc_mgr_config_path  # Parameters
-        ]
+        ],
     )
 
     # SLAM Toolbox node in async mode
-    slam_toolbox_node = LifecycleNode(          
-          package='slam_toolbox',
-          executable='async_slam_toolbox_node',
-          namespace='',
-          name='slam_toolbox',
-          output='screen',
-          parameters=[
+    slam_toolbox_node = LifecycleNode(
+        package="slam_toolbox",
+        executable="async_slam_toolbox_node",
+        namespace="",
+        name="slam_toolbox",
+        output="screen",
+        parameters=[
             # YAML files
-            slam_config_path # Parameters
-          ],
-          remappings=[
-              ('/scan', '/ldlidar_node/scan')
-          ]          
+            slam_config_path,  # Parameters
+            {
+                "use_lifecycle_manager": use_lifecycle_manager,
+                "use_sim_time": use_sim_time,
+            },
+        ],
+        remappings=[("/scan", "/ldlidar_node/scan")],
     )
 
     # Include LDLidar launch
     ldlidar_launch = IncludeLaunchDescription(
-        launch_description_source=PythonLaunchDescriptionSource([
-            get_package_share_directory('ldlidar_node'),
-            '/launch/ldlidar.launch.py'
-        ]),
-        launch_arguments={
-            'node_name': 'ldlidar_node'
-        }.items()
+        launch_description_source=PythonLaunchDescriptionSource(
+            [get_package_share_directory("ldlidar_node"), "/launch/ldlidar.launch.py"]
+        ),
+        launch_arguments={"node_name": "ldlidar_node"}.items(),
     )
 
     # Fake odom publisher
     fake_odom = Node(
-        package='tf2_ros',
-        executable='static_transform_publisher',
-        name='static_transform_publisher',
-        output='screen',
-        arguments=['0', '0', '0', '0', '0', '0', 'odom', 'ldlidar_base']
+        package="tf2_ros",
+        executable="static_transform_publisher",
+        name="static_transform_publisher",
+        output="screen",
+        arguments=["0", "0", "0", "0", "0", "0", "odom", "ldlidar_base"],
     )
 
     # RVIZ2 settings
     rviz2_config = os.path.join(
-        get_package_share_directory('ldlidar_node'),
-        'config',
-        'ldlidar_slam.rviz'
+        get_package_share_directory("ldlidar_node"), "config", "ldlidar_slam.rviz"
     )
 
     # RVIZ2node
     rviz2_node = Node(
-        package='rviz2',
-        executable='rviz2',
-        name='rviz2',
-        output='screen',
-        arguments=[["-d"], [rviz2_config]]
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        output="screen",
+        arguments=[["-d"], [rviz2_config]],
+    )
+
+    configure_event = EmitEvent(
+        event=ChangeState(
+            lifecycle_node_matcher=matches_action(slam_toolbox_node),
+            transition_id=Transition.TRANSITION_CONFIGURE,
+        ),
+        condition=IfCondition(
+            AndSubstitution(autostart, NotSubstitution(use_lifecycle_manager))
+        ),
+    )
+
+    activate_event = RegisterEventHandler(
+        OnStateTransition(
+            target_lifecycle_node=slam_toolbox_node,
+            start_state="configuring",
+            goal_state="inactive",
+            entities=[
+                LogInfo(msg="[LifecycleLaunch] Slamtoolbox node is activating."),
+                EmitEvent(
+                    event=ChangeState(
+                        lifecycle_node_matcher=matches_action(slam_toolbox_node),
+                        transition_id=Transition.TRANSITION_ACTIVATE,
+                    )
+                ),
+            ],
+        ),
+        condition=IfCondition(
+            AndSubstitution(autostart, NotSubstitution(use_lifecycle_manager))
+        ),
     )
 
     # Define LaunchDescription variable
     ld = LaunchDescription()
 
+    ld.add_action(declare_autostart_cmd)
+    ld.add_action(declare_use_lifecycle_manager)
+    ld.add_action(declare_use_sim_time_argument)
+
     # Launch Nav2 Lifecycle Manager
     ld.add_action(lc_mgr_node)
-
-    # Launch SLAM Toolbox node
-    ld.add_action(slam_toolbox_node)    
 
     # Launch fake odom publisher node
     ld.add_action(fake_odom)
@@ -122,5 +175,10 @@ def generate_launch_description():
 
     # Start RVIZ2
     ld.add_action(rviz2_node)
+
+    # Launch SLAM Toolbox node
+    ld.add_action(slam_toolbox_node)
+    ld.add_action(configure_event)
+    ld.add_action(activate_event)
 
     return ld
